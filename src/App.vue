2f27c34286d3930c72ab26716f9ce8b80e1f5629
hillarysousa <hillaryhpf@gmail.com>
2017-07-21 02:47:19 -0300

    Mudança de configuração inicial do app. Webpack, Vue, Vue Router, Babelm ESLint e Express instalados, dentre outros. Hello World criado com Vue.

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <message>Hello World!</message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue'
+
+export default {
+  name: 'app',
+  components: {Message}
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box-info">
+    <h1>
+      <slot></slot>
+    </h1>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+  h1 {
+    color: #157CBD;
+  }
+</style>
